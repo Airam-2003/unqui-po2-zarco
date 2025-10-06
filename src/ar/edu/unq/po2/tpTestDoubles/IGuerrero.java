3b020ea3d91f43a8ccfb54ade41a47716b02e90f
Julián Zarco <julianzarco.2003@outlook.com>
2025-10-06 18:26:12 -0300

    Cambios

--- src/ar/edu/unq/po2/tpTestDoubles/IGuerrero.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unq.po2.tpTestDoubles;
-
-public interface IGuerrero {
-	public void atacar(IGuerrero unGuerrero);
-	public void descontarVida(Integer unosPuntosDeVida);
-}
--- src/ar/edu/unq/po2/tpTestDoubles/IMoneda.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.unq.po2.tpTestDoubles;
-
-public interface IMoneda {
-	public void sortear();
-	public Boolean esCara();
-	public Boolean esCruz();
-}
